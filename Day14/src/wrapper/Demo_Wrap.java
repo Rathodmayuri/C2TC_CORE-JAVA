9f3853a516fd3b5ba11f63b8a2743849ff5bbc60
Mayuri <Mayuri@LAPTOP-M6C8PQ3S>
2022-04-24 16:43:12 +0530

    Firstcommit

---
+++ Day14/src/wrapper/Demo_Wrap.java
@@ -0,0 +1,16 @@
+package wrapper;
+
+public class Demo_Wrap {
+
+	public static void main(String[] args) {
+		
+		int i=30;
+		Integer a=i;
+		Integer j=new Integer(15);
+		System.out.println(a+" "+i+" "+j);
+		//System.out.println(+a);
+		//System.out.println(+j);	
+		//System.out.println(+i);
+	}
+
+}
