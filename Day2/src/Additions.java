55eea39f93ce75dd6d834d689f6b61ecdb372cd6
Mayuri <Mayuri@LAPTOP-M6C8PQ3S>
2022-04-27 18:18:06 +0530

    javacollection

---
+++ Day2/src/Additions.java
@@ -0,0 +1,20 @@
+
+public class Additions {
+
+import java.util.Scanner;
+
+public static void main(String[] args) { 
+		
+		int a, b, c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number: ");
+		a = sc.nextInt();
+		
+		System.out.println("Enter Second Number: ");
+		b = sc.nextInt();
+		
+		sc.close();
+		c = a + b;
+		System.out.println("The Sum of these numbers: "+c);
+	}
+
